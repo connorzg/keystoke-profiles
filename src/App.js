37879c7945761fda35effc05ca82c9223d34a1b0
Connor Garber <connor.garber@gmail.com>
2017-01-31 17:10:28 -0600

    Basic user data editing implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import AuthService from './utils/AuthService';
-import ProfilePage from './ProfilePage';
+import Login from './Login';
 import Home from './Home';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
       idToken: null
      };
   }
 
   componentWillMount() {
     this.auth = new AuthService('w1gVQ10geuWCt2wQrQ1yLR7kmucFsxcB', 'connorzg.auth0.com');
-
     if (this.auth.loggedIn()) {
-      let idToken = this.auth.getToken();
+      const idToken = this.auth.getToken();
       this.setState({ idToken });
     }
   }
@@ -25,7 +23,7 @@ class App extends Component {
     if (this.state.idToken) {
       return (<Home auth={this.auth} idToken={this.state.idToken} />);
     } else {
-      return (<ProfilePage auth={this.auth} />)
+      return (<Login auth={this.auth} />)
     }
   }
 }
--- src/Home.js
+++ src/Home.js
@@ -8,24 +8,25 @@ class Home extends Component {
     super(props);
     this.state = {
       profile: {},
-      users: []
+      users: [],
+      editMode: false
      };
   }
 
   getAllUsers() {
-    let headers = {
-      headers: { authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1VVXpOakJETUVRNVJqTTRRa0ZCUVRWR01rVkZSalkxUlRneE1VVXdSakUyUkRJelFUWXlNZyJ9.eyJpc3MiOiJodHRwczovL2Nvbm5vcnpnLmF1dGgwLmNvbS8iLCJzdWIiOiJpQlFWbENnV0FZQWxuU2syNmNnS1ZPbTZWcGZLWDdjQkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9jb25ub3J6Zy5hdXRoMC5jb20vYXBpL3YyLyIsImV4cCI6MTQ4NTk4MTEwNCwiaWF0IjoxNDg1ODk0NzA0LCJzY29wZSI6InJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIHJlYWQ6dXNlcl9pZHBfdG9rZW5zIHJlYWQ6dXNlcnNfYXBwX21ldGFkYXRhIHVwZGF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSJ9.mqTdRfWxaZFg0COuEKH-rDJLLWld61E7BQIV3PvhPfi7hW-u0eLTgOTM3tQuRJabdc7tdmRC_hXgRf4ROC9v-8J77j7zoy_6nElTBOicYqT27cCGDwAGyR44nIeporf6RBR2rWqgdENnh4i4YEzRND99pmjsMKT8wgTWzmgwp-KoJb2c7rSbhJLpVSljpUe0WDMCAVJkSMAB6Zp494RLtsNJ2qnjoqjkLGh_mGE5_X__Q-snJpPiZQCm8iIOtFXV48EcSNALwjvLJDq-NNDZcUs1QUpzHl_LllGjbA8hf_MqS77X-tO8khl3EiqPExXlWYKLMNHlHh1rlXeZWguW3w" }
+    const headers = {
+      headers: { authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1VVXpOakJETUVRNVJqTTRRa0ZCUVRWR01rVkZSalkxUlRneE1VVXdSakUyUkRJelFUWXlNZyJ9.eyJpc3MiOiJodHRwczovL2Nvbm5vcnpnLmF1dGgwLmNvbS8iLCJzdWIiOiJpQlFWbENnV0FZQWxuU2syNmNnS1ZPbTZWcGZLWDdjQkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9jb25ub3J6Zy5hdXRoMC5jb20vYXBpL3YyLyIsImV4cCI6MTQ4NTk4NTI1NSwiaWF0IjoxNDg1ODk4ODU1LCJzY29wZSI6InJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIHJlYWQ6dXNlcnNfYXBwX21ldGFkYXRhIHVwZGF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEifQ.Zr-AelL-unLG_nFmCPp3LsChiG3cB9M4k9nV5Fst-UVaYn7HpU1M74s6kqRU3RVblpepPRGBH6ucNoECwbTaBEJ6e8u4niLafbrnt88UZWP6g6eZlJ5XQq8uUYeWkMfFdsTAA4KQt1X3T_ssavx9MIyYwNpitzq52lH50V6yfheAKcv8gSxuoo5Rx5eR2wuVMZV-RA6M2o-XYAI6CwNKhGF4I7A88Ivh1dXf9A0dBUtjRMMrVVPd_PFkYUtAygdZX6hugNbMCo6fAjOpU0tBfcMORf9r57uyd6Uua05iZGIFfPkIo1st3O2jH2SHpiqdlVVpJ59QluUP9qecMxPNHA" }
     }
 
     // GET all users with description metadata, avoiding users on my other Auth0 instance
     axios.get('https://connorzg.auth0.com/api/v2/users?q=_exists_%3Auser_metadata.description', headers)
       .then((res) => {
-        console.log(res);
         this.setState({users: res.data})
+        console.log(res.data);
       })
   }
 
-  MetadataPatch(profile) {
+  patchMetadata(profile) {
     axios({
       method: 'PATCH',
       url: `https://connorzg.auth0.com/api/v2/users/${profile.user_id}`,
@@ -39,38 +40,60 @@ class Home extends Component {
       }
     }).then((res) => {
         console.log(res.data);
-        this.setState({profile: res.data});
-      })
+        this.setState({profile: res.data}, () => this.setState({editMode: false}));
+      });
   }
 
-  componentDidMount() {
-    // The token is passed down from the App component
-    // and used to retrieve the profile
+  handleInputChange(event) {
+    const name = event.target.name;
+    const profile = this.state.profile;
+    profile.user_metadata[name] = event.target.value;
+    this.setState({ profile });
+  }
+
+  componentWillMount() {
+    // Token generated in App.js is used to get user profile
     this.props.auth.lock.getProfile(this.props.idToken, (err, profile) => {
       console.log(profile);
-      this.setState({profile: profile});
+      this.setState({ profile });
 
       this.getAllUsers();
 
       // Patch initial metadata for new users
       if (!profile.user_metadata) {
-        this.MetadataPatch(profile);
+        this.patchMetadata(profile);
       }
+
+      this.setState({editMode: true});
     });
   }
 
   render() {
-    if (this.state.profile) {
+    let profile = this.state.profile.user_metadata || this.state.profile;
+    if (!this.state.editMode) {
       return (
         <div className="home">
           <div>
-            <img src={this.state.profile.picture} alt='user'/>
-            <h2>{this.state.profile.name}</h2>
+            <img src={profile.picture} alt='user' />
+            <h2>{profile.name}</h2>
+            <p>{profile.description}</p>
             <button onClick={this.props.auth.logout.bind(this)}>Logout</button>
           </div>
           <Users users={this.state.users} />
         </div>
       );
+    } else {
+      return (
+        <div>
+          <img src={profile.picture} alt='user' />
+          <input type="text" value={profile.name} onChange={this.handleInputChange.bind(this)} name='name' placeholder="Name" />
+          <label>
+            Describe Yourself:
+            <input type="text" placeholder="Description" value={profile.description} name="description" onChange={this.handleInputChange.bind(this)} />
+          </label>
+          <button onClick={this.patchMetadata.bind(this, this.state.profile)}>Submit</button>
+        </div>
+      )
     }
   }
 }
