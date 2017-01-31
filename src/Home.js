03c155a5187f073ae07b631c4db3097191604173
Connor Garber <connor.garber@gmail.com>
2017-01-30 22:02:53 -0600

    Get user profile via Auth0

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import './Home.css';
-import ProfilePage from './ProfilePage';
-import DATA from '../data';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = {
+      profile: {}
+     };
+  }
+
+  componentDidMount() {
+    // The token is passed down from the App component
+    // and used to retrieve the profile
+    this.props.auth.lock.getProfile(this.props.idToken, (err, profile) => {
+      if (err) {
+        console.log("Error loading the Profile", err);
+        return;
+      }
+      this.setState({profile: profile});
+    });
   }
 
   render() {
-    return (
-      <ProfilePage data={ DATA } />
-    );
+    if (this.state.profile) {
+      return (
+        <div>
+          <img src={this.state.profile.picture} />
+          <h2>Welcome {this.state.profile.nickname}</h2>
+        </div>
+      );
+    } else {
+      return (
+        <div className="loading">Loading your profile</div>
+      );
+    }
   }
+
 }
 
 export default Home;
